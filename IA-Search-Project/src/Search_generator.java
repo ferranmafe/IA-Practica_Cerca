009e6cd25092746fc0f34f86e2b0c47f668a4649
Ferran Martínez <ferran.martinez.felipe@est.fib.upc.edu>
2017-10-21 13:14:03 +0200

    Moar main class functions added

--- IA-Search-Project/src/Search_generator.java
+++ IA-Search-Project/src/Search_generator.java
@@ -1,6 +1,9 @@
 
 import IA.Gasolina.CentrosDistribucion;
 import IA.Gasolina.Gasolineras;
+import aima.search.framework.*;
+import aima.search.informed.HillClimbingSearch;
+import aima.search.informed.SimulatedAnnealingSearch;
 
 import java.util.Scanner;
 
@@ -10,6 +13,8 @@ public class Search_generator {
 
     private String local_search_algorithm;
     private int heuristic_function;
+    private int successors_function;
+    private int initial_distribution;
 
     private int distribution_centers_num;
     private int distrubution_centers_mult;
@@ -19,6 +24,8 @@ public class Search_generator {
     private int petrol_stations_seed;
 
     private static final int NUMBER_HEURISTICS = 3;
+    private static final int NUMBER_SUCCESSORS = 3;
+    private static final int NUMBER_INITIAL_DISTRIBUTIONS = 3;
 
     Search_generator() {
         String command = "continue";
@@ -68,6 +75,37 @@ public class Search_generator {
 
                 if (heuristic_command >= 0 && heuristic_command < NUMBER_HEURISTICS) {
                     this.heuristic_function = heuristic_command;
+                }
+                else {
+                    System.out.println("Wrong command.");
+                    System.out.println();
+                    command = "info";
+                }
+            }
+        }
+
+        System.out.println();
+
+        //Successors function that we are going to use
+        command = "info";
+        while (command.equals("info")) {
+            System.out.println("Enter the kind of successors that you want to use (number between [0 - " + valueOf(NUMBER_SUCCESSORS) +"]). If you don't know the successors that you can use, type " + '"' + "info" + '"' + ":");
+            command = enter_new_string();
+
+            if (command.equals("info")) {
+                show_successors_info();
+            }
+            else {
+                int successors_command;
+                try {
+                    successors_command = Integer.parseInt(command);
+                }
+                catch (Exception e){
+                    successors_command = -1;
+                }
+
+                if (successors_command >= 0 && successors_command < NUMBER_SUCCESSORS) {
+                    this.successors_function = successors_command;
                 }
                 else {
                     System.out.println("Wrong command.");
@@ -198,13 +236,80 @@ public class Search_generator {
         }
 
         System.out.println();
+        //Heuristic that we are going to use
+        command = "info";
+        while (command.equals("info")) {
+            System.out.println("Enter the initial distribution that you want to use (number between [0 - " + valueOf(NUMBER_INITIAL_DISTRIBUTIONS) +"]). If you don't know the initial distributions that you can use, type " + '"' + "info" + '"' + ":");
+            command = enter_new_string();
+
+            if (command.equals("info")) {
+                show_distributions_info();
+            }
+            else {
+                int distributions_command;
+                try {
+                    distributions_command = Integer.parseInt(command);
+                }
+                catch (Exception e){
+                    distributions_command = -1;
+                }
+
+                if (distributions_command >= 0 && distributions_command < NUMBER_HEURISTICS) {
+                    this.initial_distribution = distributions_command;
+                }
+                else {
+                    System.out.println("Wrong command.");
+                    System.out.println();
+                    command = "info";
+                }
+            }
+        }
+        System.out.println();
     }
 
     void generate_search() {
+        System.out.println("Starting to generate the search ...");
+
         CentrosDistribucion centros_distribucion = new CentrosDistribucion(this.distribution_centers_num, this.distrubution_centers_mult, this.distribution_centers_seed);
         Gasolineras gasolineras = new Gasolineras(this.petrol_stations_num, this.petrol_stations_seed);
 
-        
+        SuccessorFunction successor = null;
+        HeuristicFunction heuristic = null;
+        Problem problem = null;
+        Search search = null;
+
+        switch (this.heuristic_function) {
+            case 0:
+                //heuristic = new HeuristicFunction0();
+                break;
+        }
+
+        switch (this.successors_function) {
+            case 0:
+                //successor = new SuccessorsFunction0();
+                break;
+        }
+
+        State initial_state = new State(gasolineras, centros_distribucion);
+
+        switch (this.initial_distribution) {
+            case 0:
+                //initial_state.generate_distribution_x();
+                break;
+        }
+
+        if (this.local_search_algorithm.equals("HC")) {
+            search =  new HillClimbingSearch();
+        }
+        else if (this.local_search_algorithm.equals("SA")) {
+            //search = new SimulatedAnnealingSearch(steps, stiter, k, lamb);
+        }
+
+        //problem =  new Problem(State, successor, new SearchGoal(), heuristic);
+
+        //SearchAgent agent = new SearchAgent(problem,search);
+
+
     }
 
     String enter_new_string() {
@@ -220,5 +325,21 @@ public class Search_generator {
         System.out.println("1 ->");
         System.out.println();
 
+    }
+
+
+    void show_successors_info() {
+        System.out.println("These are the kind of successors that you can use:");
+        System.out.println("0 ->");
+        System.out.println("1 ->");
+        System.out.println();
+    }
+
+
+    void show_distributions_info() {
+        System.out.println("These are the kind of initial distributions that you can use:");
+        System.out.println("0 ->");
+        System.out.println("1 ->");
+        System.out.println();
     }
 }
